8323a7d3f30f579191ea2f2e0ee98686a89d4f5b
bluefoxmac <flori84@outlook.fr>
2022-04-26 13:53:44 +0200

    first push

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ]);
+};
+
+const App = () => {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+          name: "Luna",
+          animal: "Dog",
+          breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+          name: "Bzez naine",
+          animal: "Daronne naine",
+          breed: "Naine aux gros bzez"
+      }),
+      React.createElement(Pet, {
+          name: "Bzez daronne",
+          animal: "Daronne",
+          breed: "Gros bzez"
+      }),
+    ]);
+  };
+
+  ReactDOM.render(React.createElement(App), document.getElementById("root"));
