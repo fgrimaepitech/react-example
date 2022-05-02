f89a46b6f9e7e66184ce0627001ce12e8546cf61
bluefoxmac <flori84@outlook.fr>
2022-05-02 21:56:24 +0200

    feat: add search param and use breed list

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import React from "react";
 import { render } from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+// eslint-disable-next-line import/namespace
+import SearchParams from "./SearchParam";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Bzez naine",
-      animal: "Daronne naine",
-      breed: "Naine aux gros bzez",
-    }),
-    React.createElement(Pet, {
-      name: "Bzez daronne",
-      animal: "Daronne",
-      breed: "Gros bzez",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
